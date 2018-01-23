acad0171dc45483420df148c8c78ec9c957c1421
HeZaichang.WeRDyin <heyin223@163.com>
2018-01-23 10:03:34 +0800

    hide detector

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 export default class Resizing {
     constructor (el) {
-        this.size = 10000;
+        this.el = el
+        this.size = 10000
         this.box = document.createElement('div')
         this.expansion = document.createElement('div')
         this.shrink = document.createElement('div')
+        this.box.setAttribute('class', 'resizing-detector')
+        this.box.setAttribute('style', 'z-index: -999; visibility: hidden; width: 100%; height: 100%; margin: 0; padding: 0;')
         this.shrink.setAttribute('style', 'position: absolute; left: 0px; top: 0px; width: 100%; height: 100%; overflow: scroll;background: rgba(0,0,0,0.1)')
         this.expansion.setAttribute('style', 'position: absolute; left: 0px; top: 0px; width: 100%; height: 100%; overflow: scroll;background: rgba(0,0,0,0.1)')
         this.expansion.innerHTML = `<div style="width: ${this.size}px; height: ${this.size}px"></div>`
         this.shrink.innerHTML = `<div style="width: ${222}%; height: ${222}%"></div>`
-        el.appendChild(this.expansion)
-        el.appendChild(this.shrink)
+        this.box.appendChild(this.expansion)
+        this.box.appendChild(this.shrink)
+        this.el.appendChild(this.box)
         this.reset()
         this.expansion.addEventListener('scroll', e => {
             this.scrollTo(this.shrink, this.size, this.size) 
@@ -17,17 +21,27 @@ export default class Resizing {
         this.shrink.addEventListener('scroll', e => {
             this.scrollTo(this.expansion, this.size, this.size)
         })
-        
     }
     reset () {
         this.scrollTo(this.expansion, this.size, this.size)
         this.scrollTo(this.shrink, this.size, this.size)        
     }
-    on (cb) {
-        this.expansion.addEventListener('scroll', cb)
-    }
     scrollTo(el, top, left) {
         el.scrollTop = top
         el.scrollLeft = left
+    }
+    on (cb) {
+        this.expansion.addEventListener('scroll', cb)
+    }
+    off (cb) {
+        this.expansion.removeEventListener('scroll', cb)
+    }
+    destroy () {
+        this.el.removeChild(this.box)
+        // TODO: remove eventListener ?
+        this.el = null
+        this.box = null
+        this.shrink = null
+        this.expansion = null
     }
 }
